a4b99462083fa0d2f900fe86b53f7fac22028106
omeramin <omeramin-git@outlook.com>
2017-05-14 22:31:25 -0400

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace EventHubStreamer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_connectEventHub = new System.Windows.Forms.Button();
+            this.updateTextBoxbackgroundWorker = new System.ComponentModel.BackgroundWorker();
+            this.btn_closeEventHub = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 37);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(1043, 616);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.WordWrap = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Status";
+            // 
+            // btn_connectEventHub
+            // 
+            this.btn_connectEventHub.Location = new System.Drawing.Point(899, 8);
+            this.btn_connectEventHub.Name = "btn_connectEventHub";
+            this.btn_connectEventHub.Size = new System.Drawing.Size(75, 23);
+            this.btn_connectEventHub.TabIndex = 2;
+            this.btn_connectEventHub.Text = "Connect";
+            this.btn_connectEventHub.UseVisualStyleBackColor = true;
+            this.btn_connectEventHub.Click += new System.EventHandler(this.btn_connectEventHub_Click);
+            // 
+            // updateTextBoxbackgroundWorker
+            // 
+            this.updateTextBoxbackgroundWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.updateTextBoxbackgroundWorker_DoWork);
+            this.updateTextBoxbackgroundWorker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.updateTextBoxbackgroundWorker_ProgressChanged);
+            // 
+            // btn_closeEventHub
+            // 
+            this.btn_closeEventHub.Enabled = false;
+            this.btn_closeEventHub.Location = new System.Drawing.Point(980, 8);
+            this.btn_closeEventHub.Name = "btn_closeEventHub";
+            this.btn_closeEventHub.Size = new System.Drawing.Size(75, 23);
+            this.btn_closeEventHub.TabIndex = 3;
+            this.btn_closeEventHub.Text = "Close";
+            this.btn_closeEventHub.UseVisualStyleBackColor = true;
+            this.btn_closeEventHub.Click += new System.EventHandler(this.btn_closeEventHub_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 665);
+            this.Controls.Add(this.btn_closeEventHub);
+            this.Controls.Add(this.btn_connectEventHub);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_connectEventHub;
+        private System.ComponentModel.BackgroundWorker updateTextBoxbackgroundWorker;
+        private System.Windows.Forms.Button btn_closeEventHub;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,129 @@
+﻿using Microsoft.Azure.EventHubs;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EventHubStreamer
+{
+    public partial class Form1 : Form
+    {
+        private static string sbURI = "sb://example.servicebus.windows.net";//ServiceBus URL - i.e. "sb://example.servicebus.windows.net"
+        private static string eventHubName = "ingest";//Event Hub Name
+        private static string SASKeyName = "RootManageSharedAccessKey";//SAS Key Name - i.e. "RootManageSharedAccessKey"
+        private static string SASKey = "8jRh0iAMn7Lehwit73nf7QZJVFzOQ75DPIJzEcM0d+E=";//SAS Key Value - "kjd43298782nbjksdfuywerbdfsduoiewr"
+        private static EventHubClient ehClient;
+        private static PartitionReceiver receiver;
+
+        public delegate void SetTextCallback(string str);
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_connectEventHub_Click(object sender, EventArgs e)
+        {
+            btn_connectEventHub.Enabled = false;
+            btn_closeEventHub.Enabled = true;
+
+            label1.Text = "Connecting to Hub";
+            Uri uri = new Uri(sbURI);
+            EventHubsConnectionStringBuilder connectionString = new EventHubsConnectionStringBuilder(uri, eventHubName, SASKeyName, SASKey);
+            ehClient = EventHubClient.CreateFromConnectionString(connectionString.ToString());
+            receiver = ehClient.CreateReceiver("receiver1", "1", null);
+
+            label1.Text = "Connected to Hub,";
+            //Pick one of the following methods
+            //**Method 1 - Uses a Task object to receive events, and updates the TextBox using the BeginInvoke method.
+            //**Method 2 - Uses a BackgroundWorker to receive events, updates UI thread ProgressChanged EventHandler pattern.
+
+            //Method1();
+            Method2();
+            
+        }
+
+        private void Method1()
+        {
+            SetTextCallback d = new SetTextCallback(SetText);
+
+            Task task = new Task(() => {
+
+                do
+                {
+                    IEnumerable<EventData> events = receiver.ReceiveAsync(1000).Result;
+
+                    if (events != null)
+                    {
+                        string str = "";
+                        foreach (EventData ev in events)
+                        {
+
+                            str += ev.SystemProperties.SequenceNumber + " - " + ev.SystemProperties.Offset + ", " + Encoding.UTF8.GetString(ev.Body.ToArray()) + "\r\n";
+                        }
+
+                        textBox1.BeginInvoke(d, str);
+
+                    }
+                } while (true);
+
+            });
+
+            task.Start();
+
+
+        }
+
+        private void SetText(string str)
+        {
+            textBox1.AppendText(str);
+        }
+
+        private void Method2()
+        {
+            updateTextBoxbackgroundWorker.WorkerReportsProgress = true;
+            updateTextBoxbackgroundWorker.RunWorkerAsync();
+        }
+
+        private void updateTextBoxbackgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            do
+            {
+                IEnumerable<EventData> events = receiver.ReceiveAsync(1000).Result;
+                if (events != null)
+                    updateTextBoxbackgroundWorker.ReportProgress(50, events);
+
+            } while (true);
+
+        }
+
+        private void updateTextBoxbackgroundWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            string str = "";
+            foreach (EventData ev in (IEnumerable<EventData>)e.UserState)
+            {
+                str += ev.SystemProperties.SequenceNumber + " - " + ev.SystemProperties.Offset + ", " + Encoding.UTF8.GetString(ev.Body.ToArray()) + "\r\n";
+            }
+
+            textBox1.AppendText(str);
+
+        }
+
+        private void btn_closeEventHub_Click(object sender, EventArgs e)
+        {
+            if (ehClient != null)
+            {
+                ehClient.Close();
+            }
+
+            btn_closeEventHub.Enabled = false;
+            btn_connectEventHub.Enabled = true;
+        }
+    }
+}
